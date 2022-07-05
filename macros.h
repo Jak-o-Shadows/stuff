62e6e5e154ee3b2f4600a855965a68bbe4f16e2d
Jak_o_Shadows <jak-o-shadows@users.noreply.github.com>
2022-07-05 21:18:57 +0930

    2x uint8 --> int16 macro

--- macros.h
+++ macros.h
@@ -5,5 +5,7 @@
 #define ClearBit(BIT, PORT) PORT &= ~(1 << BIT)
 #define InvertBit(BIT, PORT) PORT ^= (1 << BIT)
 
+#define JoinInt8(BYTEH, BYTEL) ( ((int16_t) BYTEH << 8) | BYTEL )
+
 // Specific to the comms protocol
 #define PackBits(V, P) (((V << 5) & 0xFF00) | (V & 0x07) | 0x8000 | (P << 3))
